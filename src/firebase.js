bfed05048d6cec9ce5bd91cc0bab9bf2b9bb5fa6
Magnus Brandt <magnusbrandt85@gmail.com>
2022-04-26 16:43:23 +0200

    lite bråte

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
@@ -14,6 +13,5 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-export const analytics = getAnalytics(app)
 export const auth = getAuth(app)
 export const firestore = getFirestore(app)
