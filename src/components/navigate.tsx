e45a07c7aa65d1a30dd803417ea79fd5ff17d007
ljwedin <ljwedin@gmail.com>
2023-09-26 09:55:39 +0200

    replaces calendar icon with house on button to return to today's date

--- src/components/navigate.tsx
+++ src/components/navigate.tsx
@@ -1,7 +1,7 @@
 import {
-  faAngleLeft,
-  faAngleRight,
-  faCalendarDay,
+    faAngleLeft,
+    faAngleRight,
+    faHouse,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
@@ -12,83 +12,83 @@ import { fadeIn } from '../utils/keyframes';
 import { getWeek, lastWeek, nextWeek, parseFullDate } from '../utils/week';
 
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  animation-name: ${fadeIn};
-  -webkit-animation-name: ${fadeIn};
-  animation-duration: 0.3s;
-  -webkit-animation-duration: 0.3s;
-  animation-timing-function: ease-in;
-  -webkit-animation-timing-function: ease-in;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    animation-name: ${fadeIn};
+    -webkit-animation-name: ${fadeIn};
+    animation-duration: 0.3s;
+    -webkit-animation-duration: 0.3s;
+    animation-timing-function: ease-in;
+    -webkit-animation-timing-function: ease-in;
 
-  @media screen and (max-width: 500px) {
-    position: fixed;
-    bottom: 0;
-  }
+    @media screen and (max-width: 500px) {
+        position: fixed;
+        bottom: 0;
+    }
 
-  @media screen and (min-width: 501px) {
-    padding: 0 1rem 1rem 1rem;
-  }
+    @media screen and (min-width: 501px) {
+        padding: 0 1rem 1rem 1rem;
+    }
 `;
 
 const Button = styled.span<{ next?: boolean; home?: boolean }>`
-  &.dark {
-    background-color: #3e3277;
-  }
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 3rem;
-  height: 3rem;
-  border-radius: ${({ next, home }) =>
-    next ? '.5rem 50% 50% .5rem' : home ? '.5rem' : '50% .5rem .5rem 50%'};
-  background-color: rgb(6, 155, 229);
-  font-size: 1.25rem;
-  color: white;
-  cursor: pointer;
-
-  @media screen and (max-width: 500px) {
+    &.dark {
+        background-color: #3e3277;
+    }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 3rem;
+    height: 3rem;
     border-radius: ${({ next, home }) =>
-      next ? '1rem 0 0 0' : home ? '1rem 1rem 0 0' : '0 1rem 0 0'};
-    box-shadow: 0 0 0.25rem rgba(0, 0, 0, 25%);
-  }
+        next ? '.5rem 50% 50% .5rem' : home ? '.5rem' : '50% .5rem .5rem 50%'};
+    background-color: rgb(6, 155, 229);
+    font-size: 1.25rem;
+    color: white;
+    cursor: pointer;
 
-  @media screen and (min-width: 501px) {
-    box-shadow: 0 0.25rem 0.25rem rgba(0, 0, 0, 15%);
-  }
+    @media screen and (max-width: 500px) {
+        border-radius: ${({ next, home }) =>
+            next ? '1rem 0 0 0' : home ? '1rem 1rem 0 0' : '0 1rem 0 0'};
+        box-shadow: 0 0 0.25rem rgba(0, 0, 0, 25%);
+    }
+
+    @media screen and (min-width: 501px) {
+        box-shadow: 0 0.25rem 0.25rem rgba(0, 0, 0, 15%);
+    }
 `;
 
 export default function Navigate() {
-  const { week, setWeek, lightmode } = useAppState();
-  const [show, setShow] = useState<boolean>(false);
+    const { week, setWeek, lightmode } = useAppState();
+    const [show, setShow] = useState<boolean>(false);
 
-  const handleLast = () => setWeek(lastWeek(week[0].date));
-  const handleNext = () => setWeek(nextWeek(week[6].date));
-  const handleCurrent = () => setWeek(getWeek());
+    const handleLast = () => setWeek(lastWeek(week[0].date));
+    const handleNext = () => setWeek(nextWeek(week[6].date));
+    const handleCurrent = () => setWeek(getWeek());
 
-  useEffect(() => {
-    const currentDate = parseFullDate(new Date());
-    if (week.filter(({ date }) => date === currentDate).length > 0)
-      setShow(false);
-    else setShow(true);
-  }, [week]);
+    useEffect(() => {
+        const currentDate = parseFullDate(new Date());
+        if (week.filter(({ date }) => date === currentDate).length > 0)
+            setShow(false);
+        else setShow(true);
+    }, [week]);
 
-  return (
-    <Container>
-      <Button className={lightmode} onClick={handleLast}>
-        <FontAwesomeIcon icon={faAngleLeft} />
-      </Button>
+    return (
+        <Container>
+            <Button className={lightmode} onClick={handleLast}>
+                <FontAwesomeIcon icon={faAngleLeft} />
+            </Button>
 
-      {show && (
-        <Button className={lightmode} home onClick={handleCurrent}>
-          <FontAwesomeIcon icon={faCalendarDay} />
-        </Button>
-      )}
+            {show && (
+                <Button className={lightmode} home onClick={handleCurrent}>
+                    <FontAwesomeIcon icon={faHouse} />
+                </Button>
+            )}
 
-      <Button className={lightmode} next onClick={handleNext}>
-        <FontAwesomeIcon icon={faAngleRight} />
-      </Button>
-    </Container>
-  );
+            <Button className={lightmode} next onClick={handleNext}>
+                <FontAwesomeIcon icon={faAngleRight} />
+            </Button>
+        </Container>
+    );
 }
